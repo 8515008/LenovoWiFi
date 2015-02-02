8fb774f6529da13fc0c5e36df8fa0465fbf11f55
Kidd Liu <kiddliu.sz@gmail.com>
2015-02-02 15:40:27 +0800

    Dynamic Loading Service

--- LenovoWiFiWCFService/WindowsService.cs
+++ LenovoWiFiWCFService/WindowsService.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using System.ServiceModel;
 using System.ServiceProcess;
+using System.Threading.Tasks;
 
 namespace Lenovo.WiFi
 {
     partial class WindowsService : ServiceBase
     {
+        private const string ServiceLibrary = "LenovoWiFiWCFLibrary";
+        private const string ServiceType = "Lenovo.WiFi.HostedNetworkService";
+
         private ServiceHost _serviceHost;
 
         public WindowsService()
@@ -20,8 +26,35 @@ namespace Lenovo.WiFi
                 _serviceHost.Close();
             }
 
-            _serviceHost = new ServiceHost(typeof(HostedNetworkService));
-            _serviceHost.Open();
+            var rootDirectory = Environment.CurrentDirectory;
+
+            var latestVersion = new Version(-1, 0);
+            foreach (var subDirectory in new DirectoryInfo(rootDirectory).GetDirectories())
+            {
+                var version = new Version(subDirectory.Name);
+                if (version > latestVersion)
+                {
+                    Environment.CurrentDirectory = subDirectory.FullName;
+                }
+            }
+
+            Task.Run(() =>
+            {
+                // TODO: CHECK FOR NEW VERSION
+                // TODO: DOWNLOAD NEW VERSION IF NECESSARY
+            });
+
+            if (Environment.CurrentDirectory != rootDirectory)
+            {
+                var assembly = Assembly.LoadFrom(ServiceLibrary);
+                var service = assembly.GetType(ServiceType);
+
+                if (service != null)
+                {
+                    _serviceHost = new ServiceHost(service);
+                    _serviceHost.Open();
+                }
+            }
         }
 
         protected override void OnStop()
